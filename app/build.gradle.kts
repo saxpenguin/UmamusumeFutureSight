7bbfac4808d4b3c58fd477c90a4312317e26fd70
ping.hsu <ping.hsu@dhtcl.com>
2026-02-03 16:04:52 +0800

    feat: Update version code and version name to 2 and 1.0.1 respectively

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.saxpenguin.umamusumefuturesight"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "com.saxpenguin.umamusumefuturesight.HiltTestRunner"
         vectorDrawables {
